2f12dd72a62ded4060400d97337f9bd0de3ee3aa
pro100proger <nazar4075066@gmail.com>
2025-03-01 12:11:49 +0200

    added tasks descriptions

--- src/Tasks/MergeSortedArrays.java
+++ src/Tasks/MergeSortedArrays.java
@@ -3,6 +3,18 @@ package Tasks;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
+/*
+        Task 1
+    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+    and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+    The final sorted array should not be returned by the function, but instead be
+    stored inside the array nums1. To accommodate this, nums1 has a length of m + n,
+    where the first m elements denote the elements that should be merged,
+    and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ */
 public class MergeSortedArrays {
     ////      First variant
     //        public static void merge(int[] nums1, int m, int[] nums2, int n) {
