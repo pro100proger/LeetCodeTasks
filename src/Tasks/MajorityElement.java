ca4eed45fc35d5a63535d79285fd90e802071e57
pro100proger <nazar4075066@gmail.com>
2025-03-03 12:44:08 +0200

    solved majority element task

--- src/Tasks/MajorityElement.java
+++ src/Tasks/MajorityElement.java
@@ -7,7 +7,45 @@ package Tasks;
     You may assume that the majority element always exists in the array.
  */
 public class MajorityElement {
-    public int majorityElement(int[] nums) {
-        return 1;
+    // Boyer-Moore Voting Algorithm
+    public static int majorityElement(int[] nums) {
+        int majorityElement = nums[0], numberOfMajorityElement = 0;
+        for (int num : nums) {
+            if (numberOfMajorityElement == 0) {
+                majorityElement = num;
+            }
+
+            if (num == majorityElement) {
+                numberOfMajorityElement++;
+            } else {
+                numberOfMajorityElement--;
+            }
+        }
+
+        return majorityElement;
     }
+
+    // Second variant with bad time complexity O(n^2)
+    //    public static int majorityElement(int[] nums) {
+    //        int majorityElement = nums[0];
+    //        int numberOfMajorityElement = 0;
+    //        for (int i = 0; i < nums.length; i++) {
+    //            int currentNumberOfElement = 0;
+    //            for (int j = 1; j < nums.length; j++) {
+    //                if (nums[i] == nums[j]) {
+    //                    currentNumberOfElement++;
+    //                }
+    //            }
+    //            if (currentNumberOfElement > numberOfMajorityElement) {
+    //                majorityElement = nums[i];
+    //                numberOfMajorityElement = currentNumberOfElement;
+    //            }
+    //        }
+    //        return majorityElement;
+    //    }
+
+    //    //    Code for main to test the methods
+    //    int[] nums = new int[] { 1, 3, 2, 3, 3, 1, 1, 1, 1, 3, 3, 4, 3 };
+    //    int k = majorityElement(nums);
+    //        System.out.println(k);
 }
