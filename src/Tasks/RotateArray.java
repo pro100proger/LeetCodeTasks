0fd5b61b3568d00eac48f64c234ada2fad53a8db
pro100proger <nazar4075066@gmail.com>
2025-03-04 14:11:09 +0200

    added main and optimized code

--- src/Tasks/RotateArray.java
+++ src/Tasks/RotateArray.java
@@ -18,6 +18,7 @@ import java.util.Arrays;
 public class RotateArray {
     //  First variant with bad time complexity O(n*k)
     public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
         for (int i = 0; i < k; i++) {
             int lastElement = nums[nums.length - 1];
             for (int j = nums.length - 1; j > 0; j--) {
@@ -27,4 +28,10 @@ public class RotateArray {
             System.out.println(Arrays.toString(nums));
         }
     }
+
+
+//    //    Code for main to test the methods
+//    int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+//    int k = 3;
+//    rotate(nums, k);
 }
