baf67cc9a1f4138a033c0cd0305dcd41fb87296a
pro100proger <nazar4075066@gmail.com>
2025-03-04 13:53:19 +0200

    added description to task 6

---
+++ src/Tasks/RotateArray.java
@@ -0,0 +1,20 @@
+package Tasks;
+
+/*
+        Task 6
+    Given an integer array nums, rotate the array
+    to the right by k steps, where k is non-negative.
+
+    Example:
+    Input: nums = [1,2,3,4,5,6,7], k = 3
+    Output: [5,6,7,1,2,3,4]
+    Explanation:
+    rotate 1 steps to the right: [7,1,2,3,4,5,6]
+    rotate 2 steps to the right: [6,7,1,2,3,4,5]
+    rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ */
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+
+    }
+}
