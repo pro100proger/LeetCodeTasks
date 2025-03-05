630eeace79364c07ec99d96f25ca2fb9da24ac21
pro100proger <nazar4075066@gmail.com>
2025-03-05 14:45:43 +0200

    solved best time to buy and sell stock task

--- src/Tasks/BestTimeToBuyAndSellStock.java
+++ src/Tasks/BestTimeToBuyAndSellStock.java
@@ -14,6 +14,33 @@ package Tasks;
  */
 public class BestTimeToBuyAndSellStock {
     public static int maxProfit(int[] prices) {
-        return 0;
+        int minPrice = prices[0];
+        int minDay = 1;
+        int maxProfit = 0;
+        int buyDay = 1, sellDay = 1;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+                minDay = i + 1;
+            }
+
+            int currentProfit = prices[i] - minPrice;
+
+            if (currentProfit > maxProfit) {
+                maxProfit = currentProfit;
+                buyDay = minDay;
+                sellDay = i + 1;
+            }
+        }
+
+        if (maxProfit > 0) {
+            System.out.printf("Buy on day %d (price = %d) and sell on day %d (price = %d), profit = %d.%n",
+                buyDay, prices[buyDay - 1], sellDay, prices[sellDay - 1], maxProfit);
+        } else {
+            System.out.println("No profitable transaction possible.");
+        }
+
+        return maxProfit;
     }
 }
