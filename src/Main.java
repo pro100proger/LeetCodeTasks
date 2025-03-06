5040d174428648066e1b32b7822508f5feeabd8e
pro100proger <nazar4075066@gmail.com>
2025-03-06 12:48:04 +0200

    added description to task 9

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 
 import static Tasks.IIBestTimeToBuyAndSellStock.maxProfit;
+import static Tasks.JumpGame.canJump;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] prices = new int[] { 1,5,7,3,6,9 };
-        int k = maxProfit(prices);
+        int[] nums = new int[] { 1,5,7,3,6,9 };
+        boolean k = canJump(nums);
         System.out.println(k);
     }
 }
---
+++ src/Tasks/JumpGame.java
@@ -0,0 +1,20 @@
+package Tasks;
+
+/*
+        Task 9
+    You are given an integer array nums. You are
+    initially positioned at the array's first index,
+    and each element in the array represents your
+    maximum jump length at that position.
+    Return true if you can reach the last index, or false otherwise.
+
+    Example:
+    Input: nums = [2,3,1,1,4]
+    Output: true
+    Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ */
+public class JumpGame {
+    public static boolean canJump(int[] nums) {
+        return true;
+    }
+}
