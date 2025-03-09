3b1481099e3514c69819347d874cfad8049b85a7
pro100proger <nazar4075066@gmail.com>
2025-03-09 20:49:30 +0200

    added description to task 11

---
+++ src/Tasks/HIndex.java
@@ -0,0 +1,27 @@
+package Tasks;
+
+/*
+        Task 11
+    Given an array of integers citations where citations[i]
+    is the number of citations a researcher received for
+    their ith paper, return the researcher's h-index.
+
+    According to the definition of h-index on Wikipedia:
+    The h-index is defined as the maximum value of h such
+    that the given researcher has published at least h
+    papers that have each been cited at least h times.
+
+    Example:
+    Input: citations = [3,0,6,1,5]      Output: 3
+    Explanation: [3,0,6,1,5] means the researcher has 5
+    papers in total and each of them had received
+    3, 0, 6, 1, 5 citations respectively.
+    Since the researcher has 3 papers with at least 3
+    citations each and the remaining two with no more
+    than 3 citations each, their h-index is 3.
+ */
+public class HIndex {
+    public int hIndex(int[] citations) {
+        return 1;
+    }
+}
