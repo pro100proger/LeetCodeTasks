9821c5a7b3e294a149eccaabe71ec6b3c316160f
pro100proger <nazar4075066@gmail.com>
2025-03-17 13:17:31 +0200

    add code for main to task 12

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
 
-import static Tasks.HIndex.hIndex;
-import static Tasks.IIJumpGame.jump;
+import Tasks.InsertDeleteGetRandomO1;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] citations = {2,9,7,3,2,6};
-        int h = hIndex(citations);
-        System.out.println(h);
+        InsertDeleteGetRandomO1 obj = new InsertDeleteGetRandomO1();
+        obj.insert(1);
+        obj.insert(2);
+        obj.insert(4);
+        obj.insert(11);
+        obj.insert(12);
+        int val = 10;
+        boolean param_1 = obj.insert(val);
+        val = 17;
+        boolean param_2 = obj.remove(val);
+        int param_3 = obj.getRandom();
+        System.out.println(param_1);
+        System.out.println(param_2);
+        System.out.println(param_3);
     }
 }
--- src/Tasks/InsertDeleteGetRandomO1.java
+++ src/Tasks/InsertDeleteGetRandomO1.java
@@ -55,4 +55,22 @@ public class InsertDeleteGetRandomO1 {
     public int getRandom() {
         return list.get(random.nextInt(list.size()));
     }
+
+    /*
+        // Code for main to test the methods
+        InsertDeleteGetRandomO1 obj = new InsertDeleteGetRandomO1();
+        obj.insert(1);
+        obj.insert(2);
+        obj.insert(4);
+        obj.insert(11);
+        obj.insert(12);
+        int val = 10;
+        boolean param_1 = obj.insert(val);
+        val = 17;
+        boolean param_2 = obj.remove(val);
+        int param_3 = obj.getRandom();
+        System.out.println(param_1);
+        System.out.println(param_2);
+        System.out.println(param_3);
+     */
 }
