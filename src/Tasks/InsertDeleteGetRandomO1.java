c6e8348d88526e98aded9197c41a03e156b68639
pro100proger <nazar4075066@gmail.com>
2025-03-17 12:58:28 +0200

    add description to task 12

---
+++ src/Tasks/InsertDeleteGetRandomO1.java
@@ -0,0 +1,36 @@
+package Tasks;
+
+/*
+        Task 12
+    Implement the RandomizedSet class:
+    1. RandomizedSet() Initializes the RandomizedSet object.
+    2. bool insert(int val) Inserts an item val into the set if
+    not present. Returns true if the item was not present, false otherwise.
+    3. bool remove(int val) Removes an item val from the set if present.
+    Returns true if the item was present, false otherwise.
+    4. int getRandom() Returns a random element from the current
+    set of elements (it's guaranteed that at least one element
+    exists when this method is called). Each element
+    must have the same probability of being returned.
+
+    You must implement the functions of the class such
+    that each function works in average O(1) time complexity.
+ */
+public class InsertDeleteGetRandomO1 {
+
+    public InsertDeleteGetRandomO1() {
+
+    }
+
+    public boolean insert(int val) {
+        return true;
+    }
+
+    public boolean remove(int val) {
+        return true;
+    }
+
+    public int getRandom() {
+        return 1;
+    }
+}
